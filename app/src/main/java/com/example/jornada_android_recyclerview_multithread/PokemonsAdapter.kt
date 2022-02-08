66c81b64c6c08a6457563b1a01050623cac924a7
pauloacf <pauloacf148@gmail.com>
2022-02-08 09:33:58 -0400

    Implementação do adapter

---
+++ app/src/main/java/com/example/jornada_android_recyclerview_multithread/PokemonsAdapter.kt
@@ -0,0 +1,25 @@
+package com.example.jornada_android_recyclerview_multithread
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+data class Pokemon(val nome: String, val imagemUrl: String)
+
+class PokemonsAdapter(val itens: List<Pokemon>) : RecyclerView.Adapter<PokemonsAdapter.ViewHolder>() {
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
