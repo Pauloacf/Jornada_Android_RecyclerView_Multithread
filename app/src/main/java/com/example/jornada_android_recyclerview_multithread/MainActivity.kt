08d6408709a94bdd36e4fbdc368b2909fc51b88f
pauloacf <pauloacf148@gmail.com>
2022-02-08 08:57:32 -0400

    Initial commit

---
+++ app/src/main/java/com/example/jornada_android_recyclerview_multithread/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.jornada_android_recyclerview_multithread
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
