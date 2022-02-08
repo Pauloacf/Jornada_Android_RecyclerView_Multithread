7faef9c89d7002ab392ca94ce5dfbe050fa2b41b
pauloacf <pauloacf148@gmail.com>
2022-02-08 10:43:04 -0400

    Implementação do app

--- app/src/main/java/com/example/jornada_android_recyclerview_multithread/MainActivity.kt
+++ app/src/main/java/com/example/jornada_android_recyclerview_multithread/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.jornada_android_recyclerview_multithread
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rvPokemons = findViewById<RecyclerView>(R.id.rvPokemons)
+
+        // Layout Manager
+        rvPokemons.layoutManager = LinearLayoutManager(this)
+
+        // Adapter
+        val charmander = Pokemon(
+            "Charmander",
+            "https://sm.ign.com/ign_br/screenshot/default/blob_bkmv.jpg"
+        )
+        val pikachu = Pokemon(
+            "Pikachu",
+            "https://cidadedoheroi.com.br/wp-content/uploads/2021/11/Pikachu-capa.jpg"
+        )
+        val listaPokemons = listOf(charmander, pikachu, charmander)
+
+        rvPokemons.adapter = PokemonsAdapter(listaPokemons)
     }
 }
